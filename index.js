4cabb8929ec4c7c46cc1fb2654ba5204472f6dda
I334178 <pedro.iranzo.egea@sap.com>
2021-01-29 12:23:35 +0100

    basic auth SCIM call

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const got = require('got');
+const axios = require('axios');
 
-(async () => {
-  try {
-    const response = await got('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true });
-    console.log(response.body.url);
-    console.log(response.body.explanation);
-  } catch (error) {
-    console.log(error.response.body);
-  }
-})();
+async function makeGetRequest() {
+
+    const config = {
+        method: 'get',
+        url: 'https://preferredsuccess-ias.accounts.ondemand.com/service/scim/Users/P000251',
+        headers: { 'Content-Type': 'application/scim+json' },
+        auth: {
+            username: 'c4e5b890-9dae-4d52-8e6c-5f0ed48f96d8',
+            password: 'ZDby7IJlZJnWqaWg7xZs1'
+        }
+    }
+
+    let res = await axios(config)
+
+    let data = res.data;
+    console.log(data);
+}
+
+makeGetRequest();
